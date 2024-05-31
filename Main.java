d8af5932336f99de106202487738a3503b5edd73
endaya14 <jelogiananendaya14@gmail.com>
2024-05-31 15:24:19 +0800

    test
    commit sa bahay

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
+  A a = new A();
+  a.x = 1;
+  a.y = 2;
+
+  System.out.println(a.sum);
+
   }
+
+
 }
+
