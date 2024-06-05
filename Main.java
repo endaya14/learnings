c1c7147e96c3d0ed5f8da8007c5025a1d71f7e25
endaya14 <jelogiananendaya14@gmail.com>
2024-06-06 06:09:27 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
-  A a = new A();
-  a.x = 1;
-  a.y = 2;
 
-  System.out.println(a.sum);
 
   }
 
